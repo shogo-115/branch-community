34580fc80fb122306f2e2cc560b5b4c04f6fac8f
shogo-115 <izumi.may.11@gmail.com>
2020-08-23 20:30:36 +0900

    画像のアップロード

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'shogo2020branchcommunity'
+    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/shogo2020branchcommunity"
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
