c421b2db6ac1f214ad2537dd914b24dc72e5f1d4
shogo-115 <izumi.may.11@gmail.com>
2020-08-23 20:21:11 +0900

    画像アップロード

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'shogo2020branchcommunity'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV['AWS_S3_REGION'],
+      path_style: true
+    }
+  else
+    config.storage :file
+  end
+end
