82db85f554a50e8f0809564c67a01559d3d7dea0
shogo-115 <izumi.may.11@gmail.com>
2020-09-07 22:55:00 +0900

    ページネーション追加

--- app/controllers/businesspages_controller.rb
+++ app/controllers/businesspages_controller.rb
@@ -1,7 +1,7 @@
 class BusinesspagesController < ApplicationController
 
   def index
-    @admins = Admin.where(category: "ビジネス")
+    @admins = Admin.where(category: "ビジネス").page(params[:page]).per(15)
   end
 
 end
