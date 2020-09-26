1bb55d9bbad4dc4f708bcd795f4f9a43574d0094
shogo-115 <izumi.may.11@gmail.com>
2020-09-26 18:01:29 +0900

    DMの非同期通信実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,39 @@
+$(function(){
+  function buildHTML(message){
+    let html =
+      `<div class="messageContents">
+        <div class="messageContents__rightMessage">
+          ${message.content}
+        </div>
+        <div class="messageContents__rightMessageDate">
+          ${message.created_at}
+        </div>
+      </div>`
+      return html;
+    };
+
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.chatContents__field').append(html);
+      $('form')[0].reset();
+      $('.chatContents__field').animate({ scrollTop: $('.chatContents__field')[0].scrollHeight});
+      $('.actionField__text').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージを100文字以内で入力してください");
+      $('.actionField__text').prop('disabled', false);
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,2 @@
+json.content @message.content
+json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
